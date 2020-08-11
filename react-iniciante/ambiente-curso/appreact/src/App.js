d0c55b1a32e967fd8edd78e6cf1faeda1ee242d3
Kelly Lima <keely06@live.com>
2020-08-11 20:41:54 -0300

    jsx array map/filter

--- react-iniciante/ambiente-curso/appreact/src/App.js
+++ react-iniciante/ambiente-curso/appreact/src/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Meu App</div>;
+  const idade = 13;
+  const produtos = ['Notebook', 'Celular', 'Teclado', 'Videogame'];
+  const livros = [
+    { nome: 'Narnia', ano: 1995 },
+    { nome: 'Senhor dos Anéis', ano: 1980 },
+    { nome: 'Anne Frank', ano: 1950 },
+  ];
+  return (
+    <React.Fragment>
+      <label htmlFor="nome">Nome</label>
+      <input type="text" id="nome" />
+      <p>{idade}</p>
+      <ul>
+        {produtos.map((produto) => (
+          <li key={produto}>{produto}</li>
+        ))}
+      </ul>
+
+      <ul>
+        {livros
+          .filter(({ ano }) => ano > 1950)
+          .map(({ nome, ano }) => (
+            <li>
+              {nome}, {ano}
+            </li>
+          ))}
+      </ul>
+    </React.Fragment>
+  );
 };
 
 export default App;
