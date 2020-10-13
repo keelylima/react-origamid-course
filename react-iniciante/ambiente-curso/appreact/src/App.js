0c4022ac87a47826ca6dac49aa2005b7ea837270
Kelly Lima <keely06@live.com>
2020-10-13 16:00:58 -0300

    add evento

--- react-iniciante/ambiente-curso/appreact/src/App.js
+++ react-iniciante/ambiente-curso/appreact/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
-const produtos = [
-  {
-    id: 1,
-    nome: 'Smartphone',
-    preco: 'R$ 2000',
-    cores: ['#29d8d5', ' #252a34', '#fc3766'],
-  },
-  {
-    id: 2,
-    nome: 'Notebook',
-    preco: 'R$ 3000',
-    cores: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    nome: 'Tablet',
-    preco: 'R$ 1500',
-    cores: ['#365069', '#47c1c8', '#f95789'],
-  },
-];
+// const produtos = [
+//   {
+//     id: 1,
+//     nome: 'Smartphone',
+//     preco: 'R$ 2000',
+//     cores: ['#29d8d5', ' #252a34', '#fc3766'],
+//   },
+//   {
+//     id: 2,
+//     nome: 'Notebook',
+//     preco: 'R$ 3000',
+//     cores: ['#ffd045', '#d4394b', '#f37c59'],
+//   },
+//   {
+//     id: 3,
+//     nome: 'Tablet',
+//     preco: 'R$ 1500',
+//     cores: ['#365069', '#47c1c8', '#f95789'],
+//   },
+// ];
 
 const App = () => {
   // const idade = 13;
@@ -30,26 +30,34 @@ const App = () => {
   //   { nome: 'Anne Frank', ano: 1950 },
   // ];
 
-  const dados = produtos.filter(
-    ({ preco }) => Number(preco.replace('R$ ', '')) > 1500,
-  );
+  // const dados = produtos.filter(
+  //   ({ preco }) => Number(preco.replace('R$ ', '')) > 1500,
+  // );
+
+  function handleClick(event) {
+    console.log(event);
+  }
   return (
-    <section>
-      {/* dados.map(dado) ... dado.id, dado.nome, dado.preco*/}
-      {dados.map(({ id, nome, preco, cores }) => (
-        <div key={id}>
-          <h1>{nome}</h1>
-          <p>Preço: {preco}</p>
-          <ul>
-            {cores.map((cor) => (
-              <li style={{ backgroundColor: cor, color: 'white' }} key={cor}>
-                {cor}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </section>
+    // AULA ARRAY
+    // <section>
+    //   {/* dados.map(dado) ... dado.id, dado.nome, dado.preco*/}
+    //   {dados.map(({ id, nome, preco, cores }) => (
+    //     <div key={id}>
+    //       <h1>{nome}</h1>
+    //       <p>Preço: {preco}</p>
+    //       <ul>
+    //         {cores.map((cor) => (
+    //           <li style={{ backgroundColor: cor, color: 'white' }} key={cor}>
+    //             {cor}
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     </div>
+    //   ))}
+    // </section>
+
+    //AULA EVENTOS
+    <button onClick={handleClick}>Clique</button>
   );
   // <React.Fragment>
   //   <label htmlFor="nome">Nome</label>
