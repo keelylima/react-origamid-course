1c6dbba01ff913b796fc68e5441bbd41686d568d
Kelly Lima <keely06@live.com>
2020-10-15 20:18:14 -0300

    atividade componente

--- react-iniciante/ambiente-curso/appreact/src/App.js
+++ react-iniciante/ambiente-curso/appreact/src/App.js
@@ -1,84 +1,24 @@
 import React from 'react';
-
-// const produtos = [
-//   {
-//     id: 1,
-//     nome: 'Smartphone',
-//     preco: 'R$ 2000',
-//     cores: ['#29d8d5', ' #252a34', '#fc3766'],
-//   },
-//   {
-//     id: 2,
-//     nome: 'Notebook',
-//     preco: 'R$ 3000',
-//     cores: ['#ffd045', '#d4394b', '#f37c59'],
-//   },
-//   {
-//     id: 3,
-//     nome: 'Tablet',
-//     preco: 'R$ 1500',
-//     cores: ['#365069', '#47c1c8', '#f95789'],
-//   },
-// ];
+import Header from './Header';
+import Home from './Home';
+import Produtos from './Produtos';
 
 const App = () => {
-  // const idade = 13;
-  // const produtos = ['Notebook', 'Celular', 'Teclado', 'Videogame'];
-  // const livros = [
-  //   { nome: 'Narnia', ano: 1995 },
-  //   { nome: 'Senhor dos Anéis', ano: 1980 },
-  //   { nome: 'Anne Frank', ano: 1950 },
-  // ];
+  const { pathname } = window.location;
 
-  // const dados = produtos.filter(
-  //   ({ preco }) => Number(preco.replace('R$ ', '')) > 1500,
-  // );
-
-  function handleClick(event) {
-    console.log(event);
+  let Pagina;
+  if (pathname === '/produtos') {
+    Pagina = Produtos;
+  } else {
+    Pagina = Home;
   }
+
   return (
-    // AULA ARRAY
-    // <section>
-    //   {/* dados.map(dado) ... dado.id, dado.nome, dado.preco*/}
-    //   {dados.map(({ id, nome, preco, cores }) => (
-    //     <div key={id}>
-    //       <h1>{nome}</h1>
-    //       <p>Preço: {preco}</p>
-    //       <ul>
-    //         {cores.map((cor) => (
-    //           <li style={{ backgroundColor: cor, color: 'white' }} key={cor}>
-    //             {cor}
-    //           </li>
-    //         ))}
-    //       </ul>
-    //     </div>
-    //   ))}
-    // </section>
-
-    //AULA EVENTOS
-    <button onClick={handleClick}>Clique</button>
+    <section>
+      <Header></Header>
+      <Pagina></Pagina>
+    </section>
   );
-  // <React.Fragment>
-  //   <label htmlFor="nome">Nome</label>
-  //   <input type="text" id="nome" />
-  //   <p>{idade}</p>
-  //   <ul>
-  //     {produtos.map((produto) => (
-  //       <li key={produto}>{produto}</li>
-  //     ))}
-  //   </ul>
-
-  //   <ul>
-  //     {livros
-  //       .filter(({ ano }) => ano > 1950)
-  //       .map(({ nome, ano }) => (
-  //         <li>
-  //           {nome}, {ano}
-  //         </li>
-  //       ))}
-  //   </ul>
-  // </React.Fragment>
 };
 
 export default App;
---
+++ react-iniciante/ambiente-curso/appreact/src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Titulo from './Titulo';
+
+const Home = () => {
+  return (
+    <section>
+      <Titulo texto="Home"></Titulo>
+      <p>Isso é a home</p>
+    </section>
+  );
+};
+
+export default Home;
